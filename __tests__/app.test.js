fea6edb7b9eba3b627119af352a7b690ede98fb5
nattalz30 <nattalz@yahoo.com>
2024-08-27 10:15:51 +0100

    Task-2

---
+++ __tests__/app.test.js
@@ -0,0 +1,31 @@
+const app = require("../app");
+const db = require("../db/connection");
+const request = require("supertest");
+const seed = require("../db/seeds/seed");
+const data = require("../db/data/test-data/index");
+const { reduce } = require("../db/data/test-data/articles");
+
+beforeEach(() => seed(data))
+afterAll(() => db.end())
+
+describe("CORE:",() => {
+    describe("Topics:", () => {
+        describe("GET:", () => {
+            it("200: returns all topics", () => {
+                return request(app)
+                .get("/api/topics")
+                .expect(200)
+                .then((response) => {
+                    const {
+                        body: { topics },
+                    } = response;
+                    topics.forEach((topic) => {
+                        expect(topic).toHaveProperty("slug");
+                        expect(topic).toHaveProperty("description");
+                    })
+                })
+            })
+
+        })
+    })
+})
